0d60d955f1b34fe3afc1fca81bfbe6693ba99cd1
Myaella <mya17plus@yahoo.ca>
2018-11-05 19:20:13 +0000

    Setting up and adding circles

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    let time = new Date().getMinutes();
+    return <div className="App">
+        <section>
+          <div className="row">
+            <div class="circle">{time}'</div>
+          </div>
+
+          <div class="circle">{time}'</div>
+          <div class="circle">{time}'</div>
+          <div className="row">
+            <div className="text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum congue, magna a bibend.{" "}
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="outer" />
+            <div className="text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum congue, magna a bibend.{" "}
+            </div>
+          </div>
+        </section>
+      </div>;
   }
 }
 
