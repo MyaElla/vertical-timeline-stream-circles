b82489a1fec1578f1d950d6e0fafa907aee359b8
Myaella <mya17plus@yahoo.ca>
2018-11-07 14:15:40 +0000

    taken out comments and unnecessary code

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ class App extends Component {
           id: 0,
           body:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-          timestamp: "18"
+          timestamp: "12"
         },
         {
           id: 1,
           body:
             "Etiam pretium orci nisi, ut malesuada neque imperdiet ut. Vestibulum blandit vehicula nunc, sit amet tempus urna fringilla eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat iaculis risus, ac tincidunt turpis efficitur vel. Pellentesque tellus tellus, vehicula id sagittis eu, lobortis eu odio.",
-          timestamp: "25"
+          timestamp: "14"
         },
         {
           id: 2,
           body: "Duis a aliquet ante. Etiam non aliquet quam.",
-          timestamp: "37"
+          timestamp: "17"
         },
         {
           id: 3,
           body:
             "Curabitur vitae efficitur ante. Nunc ornare dui dui, sed aliquam dui pulvinar at. Ut tristique purus at sem blandit, a auctor erat facilisis. Mauris et congue lorem. Ut sit amet dapibus dolor. Curabitur eget ex blandit, egestas nisl quis, blandit lorem. Etiam vitae sapien et augue imperdiet ornare vitae id nisl.",
-          timestamp: "81"
+          timestamp: "21"
         }
       ]
     };
